510d2918915eb318ac0f0355ab2d782052ab36e2
valeen-rodriguez <valentinarodriguezkittler@gmail.com>
2023-11-13 15:54:01 -0300

    Animacion finalizado

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname,'./public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=> {
     res.sendFile(path.join(__dirname,'/views/home.html'));
-})
-
-app.get('/register',(req,res)=> {
-    res.sendFile(path.join(__dirname,'/views/register.html'));
-    console.log(__dirname)
 })
 
 app.get('/login',(req,res)=> {
@@ -16,7 +16,19 @@ app.get('/login',(req,res)=> {
     console.log(__dirname)
 })
 
-app.use(express.static('public'));
+app.get('/register', (req, res) => {
+	res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.post('/register', (req,res) => {
+	console.log (req.body);
+	res.redirect('/login');
+});
+
+app.post('/login', (req,res) => {
+	console.log (req.body);
+	res.redirect('/');
+});
 
 app.listen(2000,()=> {
     console.log('Servidor ejecutandose en el puerto 2000')
